4780fcd11bbe7e2120200f0dd41bab4f9f7fbd90
IgnatenkoGD <IgnatenkoGD@yandex.ru>
2023-05-10 16:37:57 +0700

    fix style

--- src/main.c
+++ src/main.c
@@ -7,7 +7,8 @@
 #include "converter.h"
 #include "parser.h"
 
-int main() {
+int main()
+{
     char in_name[100];
     char out_name[100];
     printf("You have: ");
@@ -16,8 +17,8 @@ int main() {
     fgets(out_name, 100, stdin);
     if (error_check_inname(in_name) == 1) {
         double unit = str_to_double(in_name);
-        char *from = str_in_name(in_name);
-        char *to = str_out_name(out_name);
+        char* from = str_in_name(in_name);
+        char* to = str_out_name(out_name);
 
         unit = get_result(unit, from, to);
         printf("%lf\n", unit);
--- src/parser.h
+++ src/parser.h
@@ -1,9 +1,9 @@
-double str_to_double(char *arr);
+double str_to_double(char* arr);
 
-char *str_in_name(char *arr);
+char* str_in_name(char* arr);
 
-char *str_out_name(char *arr);
+char* str_out_name(char* arr);
 
-int error_check_inname(char *arr);
+int error_check_inname(char* arr);
 
-int convert(char *from_unit_measure, double inp_value, char *to_unit_measure);
+int convert(char* from_unit_measure, double inp_value, char* to_unit_measure);
