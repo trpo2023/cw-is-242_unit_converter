414dd93f03fda110f33aeb6ae15bc559b83d11f8
Nikulin Maxim <Flar1@users.noreply.github.com>
2023-05-11 21:34:47 +0700

    Fix codestyle

--- src/main.c
+++ src/main.c
@@ -16,8 +16,7 @@ int main()
     printf("You want: ");
     fgets(out_name, 100, stdin);
     if (error_check_inname(in_name) == 1) {
-        if (error_check_outname(out_name) == 1)
-        {
+        if (error_check_outname(out_name) == 1) {
             double unit = str_to_double(in_name);
             char* from = str_in_name(in_name);
             char* to = str_out_name(out_name);
