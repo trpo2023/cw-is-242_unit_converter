704ea220e871ca88153c2b2e0f1099061d302182
root <root@DESKTOP-BK46MSC.localdomain>
2023-05-13 16:15:14 +0700

    Delete unused functions, #14

--- src/converter.h
+++ src/converter.h
@@ -1,8 +1,7 @@
-double convert_unit(double unit, int coefficient);
-
-double get_result(double unit, char* from_unit_measure, char* to_unit_measure);
-
-typedef struct {
+typedef struct Dict {
     char* key;
     double value;
+
+    struct Dict *next;
 } Dictionary;
+
