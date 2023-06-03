6679b7a669ac167774fd0139a1161bb877b8c59d
Lyubitskiy Maxim <root@DESKTOP-BK46MSC.localdomain>
2023-06-04 01:17:11 +0700

    Update converter.h

--- src/converter.h
+++ src/converter.h
@@ -14,6 +14,8 @@ typedef struct list {
 } Listnode;
 
 double
+temp_convert(char* from_unit_measure, double inp_value, char* to_unit_measure);
+double
 convert(char* from_unit_measure, double inp_value, char* to_unit_measure);
 void list_delete(Listnode* list);
 Listnode* list_pushfront(Listnode* list, char* key, double value);
