2fe4015b2afa5bb6de81444f71a9e4bf1e2c137a
IgnatenkoGD <IgnatenkoGD@yandex.ru>
2023-06-03 19:24:51 +0700

    updated help

--- src/help.c
+++ src/help.c
@@ -2,8 +2,5 @@
 
 void pr_help()
 {
-    printf("units 62:\nmeters\ncantimeters\nmilimeters\nkilometers\nfeet\ninches\nyards\nmiles\nkilogram\npound\ngram\nmiligram\nton\nounce\nseconds\nminutes\nhour\nday\nmiliseconds\nmicroseconds\nnanoseconds\nkelvin\ncelsius\nfahr\npascal\nbar\
-\natmospheres\ntorr\njoule\nkilojoule\ngram/calorie\nkilocalori\nwatt/hour\nkilowatt/hour\nelectron/volt\nm^3\ndm^3\ncm^3\n liter\nmm^3\nkm^3\ninches^3\nfeet^3\
-\nm^2\nkm^2\nmiles^2\nyards^2\nfeet^2\ninces^2\nhectare\narc\nmeter/second\nkilometer/hour\nmile/hour\nfeet/second\nknot\ndegree\nradian\nminutes-of-angle\
-\nseconds-of-angle\nhertz\nkilohertz\nmegahertz\ngigahertz\n");
+    printf("To use unit converter you should input amount and unit type.\nUnits can be converted only if they are from the same group.\nCurrently supported units 56:\nlength (meters, cantimeters, milimeters, kilometers, feet, inches, yards, miles)\nmass (kilogram, pound, gram, miligram, ton, ounce)\ntime (seconds, minutes, hour, day, miliseconds, microseconds, nanoseconds)\ntemperature (kelvin, celsius, fahr)\npressure (pascal, bar, atmospheres,torr)\nenergy (joule, kilojoule, gram/calorie,kilocalori, watt/hour, kilowatt/hour, electron/volt)\nvolume (m^3, dm^3, cm^3, liter, mm^3, km^3, inches^3, feet^3)\narea (m^2, km^2, miles^2, yards^2, feet^2, inces^2, hectare, arc)\nspeed (meter/second, kilometer/hour, mile/hour, feet/second)\nangle (knot, degree, radian, minutes-of-angle, seconds-of-angle)\nfrequency (hertz, kilohertz, megahertz, gigahertz)\n\nAlso program support abbreviation.\nFor example you can use \"mm\" instead of \"milimeters\"\n\n");
 }
